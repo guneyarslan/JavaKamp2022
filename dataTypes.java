363dbdae9c92a9f77e9a20585ed03f55a9a4bb2d
Hasret Güney Arslan <108914279+guneyarslan@users.noreply.github.com>
2022-11-07 07:42:32 +0300

    Add files via upload

---
+++ dataTypes.java
@@ -0,0 +1,14 @@
+
+public class dataTypes {
+
+	public static void main(String[] args) {
+		boolean aciktim = true;
+		byte kacAdetPizza = 4;
+		short kacDakika = 20;
+		double kacPara = 99.90;
+		
+		
+
+	}
+
+}
---
+++ variables.java
@@ -0,0 +1,16 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+		int gelir = 5500;
+		int gider = 2200;
+		int kalanPara = (gelir-gider);
+		int telFiyat = 4000;
+
+		String mesaj1 = "Telefonu alabilirsin.";
+		String mesaj2 = "Telefonu alamazsın.";
+		
+	}
+	
+
+}
